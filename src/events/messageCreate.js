b3b61087f3d1b326e889cff92aead448ac2fc99c
zeliak <kis.oliver@outlook.hu>
2025-08-14 19:56:04 +0200

    expanding regex for better smoke detection and ignroing the size of the fire

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -3,7 +3,7 @@ const { Events } = require('discord.js');
 module.exports = {
     name: Events.MessageCreate,
     async execute(interaction) {
-        const regExp = new RegExp('smoke|smokin|fire|flame|🔥|🚬|🥵|☕|cook|toast|heat|stove')
+        const regExp = new RegExp('smoke|smokin|fire|flame|🔥|🚬|🥵|☕|cook|toast|heat|stove|inferno|combustion','i')
         if (interaction.content.match(regExp)) {
             interaction.reply('!BEEP!');
             console.log('BEEP BEEP MATHAFAKKA');
